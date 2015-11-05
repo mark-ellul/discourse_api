4b0abd96799f43fb79098fcd1a3bf8c5641c6b9b
Mark Ellul <mark@iplusd.net>
2015-11-05 18:31:56 +1100

    Added new group_members method for groups

--- lib/discourse_api/api/groups.rb
+++ lib/discourse_api/api/groups.rb
@@ -3,15 +3,15 @@ module DiscourseApi
     module Groups
       def create_group(args)
         args = API.params(args)
-                  .required(:name)
-                  .default(visible: true)
-                  .to_h
-        post("/admin/groups", args)
+        .required(:name)
+        .default(visible: true)
+        .to_h
+        post("/admin/groups.json", args)
       end
 
       def groups
         response = get("/admin/groups.json")
-        response.body
+        response[:body]
       end
 
       def group_add(group_id, users)
@@ -30,6 +30,12 @@ module DiscourseApi
 
         put("/admin/groups/#{group_id}/members.json", users)
       end
+
+      def group_members(group_id)
+        response = get("/admin/groups/#{group_id}/members.json", {limit: 1000})
+        response[:body]
+      end
+
 
       def group_remove(group_id, user)
         delete("/admin/groups/#{group_id}/members.json", user)
